3f2958962158955f0fa4391c1eb2c36e4c68d9c9
Igor Rud <rud.igor23@gmail.com>
2019-03-17 18:27:02 +0000

    Automatically backed up by Learn

---
+++ lib/cash_register.rb
@@ -0,0 +1,13 @@
+class CashRegister
+  attr_accessor :total, :discount, :price, :item
+
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item, price, quantity = 1)
+    @total = @total + price * quantity
+  end
+
+  
